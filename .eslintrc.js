93c076a8d59fc8f9876bb27e7ecea47a188f241f
JF <betaorbust@users.noreply.github.com>
2023-03-11 00:16:21 +0000

    chore: Add project scaffold stuff so eslint will work.

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	root: true, // stop looking for config files in parent directories
+	extends: ['@betaorbust/eslint-config/profiles/web-app'],
+	parserOptions: { tsconfigRootDir: __dirname },
+	rules: {
+		'import/prefer-default-export': 'off',
+		'no-console': 'off',
+		'react/no-unknown-property': ['error', { ignore: ['css'] }],
+	},
+};
